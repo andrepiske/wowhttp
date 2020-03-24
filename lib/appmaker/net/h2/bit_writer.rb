852ea061fe142414b48154ac365e63ed5cc711f1
André Diego Piske <andre@andrepiske.me>
2020-03-24 19:04:35 +0100

    Improve BitWriter performance

--- lib/appmaker/net/h2/bit_writer.rb
+++ lib/appmaker/net/h2/bit_writer.rb
@@ -1,25 +1,32 @@
 # frozen_string_literal: true
+require 'arraybuffer'
+
 module Appmaker::Net::H2
   class BitWriter
+    # Buffer size is slightly above 16k so resizing is less likely to happen
+    INITIAL_BUFFER_SIZE = 16896
+
     attr_accessor :cursor
 
     def initialize
       @cursor = 0
-      @buffer = []
+      @capacity = INITIAL_BUFFER_SIZE
+      @buffer = ArrayBuffer.new(@capacity)
+      @view = DataView.new(@buffer)
     end
 
     def bytes_array
-      bytes.pack('C*')
+      buffer_size = (@cursor >> 3)
+      buffer_size += 1 if (@cursor & 3) > 0
+      @buffer.bytes[0...buffer_size]
     end
 
-    def bytes
-      @buffer
-    end
+    alias bytes bytes_array
 
-    def write_byte by
-      (0...8).each do |index|
-        write_bit(by & (128 >> index))
-      end
+    def write_byte value
+      grow_buffer_by(1) if (@cursor >> 3) + 1 >= @capacity
+      @view.setU8(@cursor >> 3, value)
+      @cursor += 8
     end
 
     def finish_byte by
@@ -29,10 +36,19 @@ module Appmaker::Net::H2
       end
     end
 
+    def grow_buffer_by incr
+      if (@cursor >> 3) + incr >= @capacity
+        @buffer.realloc(@buffer.size + INITIAL_BUFFER_SIZE)
+        @view.size = @buffer.size
+        @capacity = @buffer.size
+      end
+    end
+
     def write_bit b
+      grow_buffer_by(1) if (@cursor >> 3) + 1 >= @capacity
+
       index = @cursor / 8
       bit_index = @cursor % 8
-      @buffer << 0 if index >= @buffer.length
       by = @buffer[index]
       by |= (128 >> bit_index) if b > 0
       @buffer[index] = by
@@ -40,21 +56,21 @@ module Appmaker::Net::H2
     end
 
     def write_int16 value
-      write_byte((value >> 8) & 0xFF)
-      write_byte(value & 0xFF)
+      grow_buffer_by(2) if (@cursor >> 3) + 2 >= @capacity
+      @view.setU16(@cursor >> 3, value)
+      @cursor += 16
     end
 
     def write_int24 value
-      write_byte((value >> 16) & 0xFF)
-      write_byte((value >> 8) & 0xFF)
-      write_byte(value & 0xFF)
+      grow_buffer_by(3) if (@cursor >> 3) + 3 >= @capacity
+      @view.setU24(@cursor >> 3, value)
+      @cursor += 24
     end
 
     def write_int32 value
-      write_byte((value >> 24) & 0xFF)
-      write_byte((value >> 16) & 0xFF)
-      write_byte((value >> 8) & 0xFF)
-      write_byte(value & 0xFF)
+      grow_buffer_by(4) if (@cursor >> 3) + 4 >= @capacity
+      @view.setU32(@cursor >> 3, value)
+      @cursor += 32
     end
 
     def write_prefixed_int value, prefix
@@ -71,22 +87,20 @@ module Appmaker::Net::H2
       end
     end
 
-    # TODO: this one is for binary data
     def write_bytes value
-      value = value.unpack('C*') if value.is_a?(String)
-      @buffer += value
-      @cursor += value.length
+      length = value.length
+      cursor = @cursor >> 3
+      grow_buffer_by(length) if cursor + length >= @capacity
+      @view.setBytes(cursor, value)
+
+      @cursor += (length << 3)
     end
 
     def write_string value
-      # TODO: optimize
       write_bit 0
       write_prefixed_int value.length, 7
 
-      # raise 'TODO: Unsupported string beyond 127bytes' if value.length >= 127
-      value.unpack('C*').each do |byte|
-        write_byte byte
-      end
+      write_bytes value
     end
   end
 end
---
+++ spec/lib/appmaker/net/h2/bit_writer_spec.rb
@@ -0,0 +1,113 @@
+require 'appmaker'
+
+describe Appmaker::Net::H2::BitWriter do
+  subject { described_class.new }
+
+  describe '#write_prefixed_int' do
+    let(:expected_bytes) do
+      [
+        0b01000000 | 5,
+        89
+      ]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_bit(0)
+      subject.write_bit(1)
+      subject.write_prefixed_int(5, 5)
+      subject.write_byte(89)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_byte' do
+    let(:expected_bytes) do
+      [0, 1, 2, 255, 127, 254]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_byte(0)
+      subject.write_byte(1)
+      subject.write_byte( 2)
+      subject.write_byte(255)
+      subject.write_byte( 127)
+      subject.write_byte(254)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_int16' do
+    let(:expected_bytes) do
+      [76, 246]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_int16(19702)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_int24' do
+    let(:expected_bytes) do
+      [28, 8, 182]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_int24(1837238)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_int32' do
+    let(:expected_bytes) do
+      [127, 255, 255, 255]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_int32(2147483647)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_bytes' do
+    let(:expected_bytes) do
+      [255, 1, 4, 0, 127, 34, 98, 150]
+    end
+    let(:bytes) do
+      "\xFF".force_encoding('ASCII-8BIT') + expected_bytes[1..-1].map(&:chr).join('')
+    end
+
+    it 'produces expected bytes' do
+      subject.write_bytes(bytes)
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  describe '#write_string' do
+    let(:expected_bytes) do
+      [13, 72, 51, 108, 76, 111, 32, 119, 48, 114, 108, 68, 33, 33]
+    end
+
+    it 'produces expected bytes' do
+      subject.write_string('H3lLo w0rlD!!')
+
+      expect(subject_bytes).to eq(expected_bytes)
+    end
+  end
+
+  private
+
+  def subject_bytes
+    array_of_bytes subject.bytes
+  end
+
+  def array_of_bytes s
+    s.split('').map &:ord
+  end
+end
